aa4396a565952de7546b7bd916288c4f5d97073e
Ronnasayd <ronnasayd@hotmail.com>
2017-09-01 19:32:08 -0300

    fix __init__

--- Mfunction.py
+++ Mfunction.py
@@ -1,23 +1,23 @@
+from math import exp,sqrt,pi
 class Mfunction:
-	from math import exp,sqrt,pi
+	
 
-	def __init__(self,flag,media,dp):
-		self.flag = flag # flag que representa o tipo de função
-		self.media = media # media da função gaussiana
-		self.dp  = dp # desvio padrao da função gaussiana
-		self.pert = None #pertinencia de ativação para um determinado valor de entrada
-
-	def __init__(self,flag,x1,y1,x2,y2,x3,y3,x4,y4):
-		self.flag = flag # flag que representa o tipo de função
-		self.x1 = x1 # abissiça ponto 1
-		self.x2 = x2 # abissiça ponto 2
-		self.x3 = x3 # abissiça ponto 3
-		self.x4 = x4 # abissiça ponto 4
-		self.y1 = y1 # ordenada ponto 1
-		self.y2 = y2 # ordenada ponto 2
-		self.y3 = y3 # ordenada ponto 3
-		self.y4 = y4 # ordenada ponto 4a
-		self.pert = None #pertinencia de ativação para um determinado valor de entrada
+	def __init__(self,flag=3,media=None,dp=None,x1=None,y1=None,x2=None,y2=None,x3=None,y3=None,x4=None,y4=None):
+		self.flag = flag
+		if self.flag == 3:
+			self.media = media # media da função gaussiana
+			self.dp  = dp # desvio padrao da função gaussiana
+			self.pert = None #pertinencia de ativação para um determinado valor de entrada
+		else:
+			self.x1 = x1 # abissiça ponto 1
+			self.x2 = x2 # abissiça ponto 2
+			self.x3 = x3 # abissiça ponto 3
+			self.x4 = x4 # abissiça ponto 4
+			self.y1 = y1 # ordenada ponto 1
+			self.y2 = y2 # ordenada ponto 2
+			self.y3 = y3 # ordenada ponto 3
+			self.y4 = y4 # ordenada ponto 4a
+			self.pert = None #pertinencia de ativação para um determinado valor de entrada
 
 	def setPertinence(self,X):
 		if self.flag == 1 or self.flag == 2:
@@ -34,7 +34,6 @@ class Mfunction:
 				self.pert = self.y4
 
 		if self.flag == 3:
-
 			self.pert = exp(-1*(((X - self.media)*(X - self.media))/(2*self.dp*self.dp)))*(1/(self.dp*sqrt(2*pi)))
 
 
--- teste.py
+++ teste.py
@@ -5,8 +5,8 @@ TRAP = 1
 TRIA = 2
 GAUS = 3
 
-lower = Mfunction(TRAP,0,0,2.5,1,5,1,5,1)
-upper = Mfunction(TRAP,0,0,2.5,0.8,5,0.8,5,0.8)
+lower = Mfunction(TRAP,None,None,0,0,2.5,1,5,1,5,1)
+upper = Mfunction(TRAP,None,None,0,0,2.5,1,5,1,5,1)
 
 Alta = IMfuzzy2(lower,upper)
 Alta.setPertinence(2)
