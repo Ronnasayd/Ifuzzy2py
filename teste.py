3d33b0381bd244a177c7cf4cc718eeb6edb095ca
Ronnasayd <ronnasayd@hotmail.com>
2017-09-01 18:36:57 -0300

    correção flags

--- teste.py
+++ teste.py
@@ -5,7 +5,7 @@ TRAP = 1
 TRIA = 2
 GAUS = 3
 
-lower = Mfunction(1,0,0,2.5,1,5,1,5,1)
-upper = Mfunction(1,0,0,2.5,0.8,5,0.8,5,0.8)
+lower = Mfunction(TRAP,0,0,2.5,1,5,1,5,1)
+upper = Mfunction(TRAP,0,0,2.5,0.8,5,0.8,5,0.8)
 
 Alta = IMfuzzy2(lower,upper)
