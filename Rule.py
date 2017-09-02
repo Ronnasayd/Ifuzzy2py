bf54957d3d903054689c6e840e9f47ab51615b7e
Ronnasayd <ronnasayd@hotmail.com>
2017-09-01 23:57:16 -0300

    classes Rules

---
+++ Rule.py
@@ -0,0 +1,4 @@
+class Rule:
+	def __init__(self,ant = None,cons = None):
+		self.antescedent = ant
+		self.consequent = cons
---
+++ Rules.py
@@ -0,0 +1,5 @@
+class Rules:
+	def __init__(self):
+		self.rules = []
+	def addRule(self,rule):
+		self.rules.append(rule)
